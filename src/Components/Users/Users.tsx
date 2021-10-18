1c1ca2cad939e7257387075b16401099e17f20af
Odinsees <pavel.o.lebedev@gmail.com>
2021-10-18 20:20:54 +0300

    create pagination for users page

--- src/Components/Users/Users.tsx
+++ src/Components/Users/Users.tsx
@@ -10,7 +10,7 @@ class Users extends React.Component<UsersPropsType> {
             axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
                 .then((response: any) => {
                     this.props.setUsers(response.data.items)
-                    this.props.setTotalUsersCount(response.data.totalCount)
+                    this.props.setTotalUsersCount(+response.data.totalCount)
                 });
     }
 
@@ -23,11 +23,8 @@ class Users extends React.Component<UsersPropsType> {
     }
 
     render() {
-
         let pagesCount = Math.ceil(this.props.totalUsersCount/this.props.pageSize)
-
         let pages = [];
-
         for (let i = 1; i <= pagesCount;i++){
             pages.push(i)
         }
